7eb7285e8f75a790d79eb45816ce3d03ec92115c
Hadi Elzayn <Hadi@huntsman-ve503-0005.apn.wlan.upenn.edu>
2018-04-02 16:06:48 -0400

    added trick recording in test strategies

--- test_strategies.py
+++ test_strategies.py
@@ -10,7 +10,7 @@ total_score_heuristic = 0
 wins_heuristic = 0.0
 wins_rando = 0.0
 ties = 0.0
-strategies = [3,2,3,2]
+#strategies = [3,2,3,2]
 
 #uncomment this block to switch to all greedy
 strategies = [2,2,2,2]
@@ -21,7 +21,7 @@ History = [[] for i in range(num_tests)];
 Winners = [[] for i in range(num_tests)];
 Bets = [[] for i in range(num_tests)];
 Scores = [[] for i in range(num_tests)];
-
+Tricks = [[] for i in range(num_tests)];
 for i in range(num_tests):
     game = Game(13, strategies)
     scores = game.playGame()
@@ -36,23 +36,29 @@ for i in range(num_tests):
     elif rando_score > heuristic_score:
         wins_rando += 1
     else:
-        ties +=1
-    
+        ties += 1
+    tricks = [-1 for i in range(4)]
+    for p in range(4):
+        tricks[p] = sum(game.T[t] == p for t in range(13))
+
     Hands[i] = game.initialHands;
     History[i] = game.h;
     Winners[i] = game.T;
     Bets[i] = game.bets;
     Scores[i] = scores;
+    Tricks[i] = tricks;
 
 tempTime = dt.datetime.now().time();
 #timeString = 'Data/Heuristic_v_Greedy' + str(dt.datetime.now().date()) + '-' + str(tempTime.hour) + '-' + str(tempTime.minute) + '-' + str(tempTime.second);
-timeString = 'Data/Greedy_v_Greedy' + str(dt.datetime.now().date()) + '-' + str(tempTime.hour) + '-' + str(tempTime.minute) + '-' + str(tempTime.second);
+#timeString = 'Data/Greedy_v_Greedy' + str(dt.datetime.now().date()) + '-' + str(tempTime.hour) + '-' + str(tempTime.minute) + '-' + str(tempTime.second);
+timeString = 'Data/Greedy_v_Greedy' #+ str(dt.datetime.now().date()) + '-' + str(tempTime.hour) + '-' + str(tempTime.minute) + '-' + str(tempTime.second);
 
 np.save(timeString + '_Hands', Hands)
 np.save(timeString + '_History', History)
 np.save(timeString + '_Winners', Winners)
 np.save(timeString + '_Bets', Bets)
 np.save(timeString + '_numTests', num_tests)
-np.save(timeString + '_Scores', scores)
+np.save(timeString + '_Scores', Scores)
+np.save(timeString + '_Tricks', Tricks)
 
 #print 'Heuristic won ' + str(wins_heuristic/num_tests*100) + '% of games, tied ' + str(ties/num_tests*100) + '% of games.'
