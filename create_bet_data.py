f9b419dd2990e3472047848f2d1a8a4ab4239b9e
Hadi Elzayn <Hadi@huntsman-ve500-0626.apn.wlan.upenn.edu>
2018-04-03 11:09:45 -0400

    new bet data creation file, appends in batches to csv

--- create_bet_data.py
+++ create_bet_data.py
@@ -1,39 +1,44 @@
 import numpy as np
-# bets = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Bets.npy')
-# scores = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Scores.npy')
-# hands = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Hands.npy')
+import csv
+from GameObject import Game
+import numpy as np
+import pandas as pd
+import datetime as dt
+import copy
+num_batches = 1000
+batch_size = 1000
+Bets = []
+Tricks = []
+strategies = [2,2,2,2]
+nameString = './Data/greedy_v_greedy_bet_data.csv'
+x_size = 26
+y_size = 1
 
-bets = np.load('./Data/Greedy_v_Greedy_Bets.npy')
-scores = np.load('./Data/Greedy_v_Greedy_Scores.npy')
-hands = np.load('./Data/Greedy_v_Greedy_Hands.npy')
-tricks = np.load('./Data/Greedy_v_Greedy_Tricks.npy')
-
-num_rounds = len(hands)
-x_data = [np.array([]) for t in range(num_rounds*4)]
-y_data = [-1 for t in range(num_rounds*4)]
-for t in range(num_rounds):
-    if t%10000==1:
-        print t
-    for p in range(4):
-        vals = [0 for i in range(13)]
-        suits = [0 for i in range(13)]
-        for c in range(13):
-            card = hands[t][p].cards[c]
-            vals[c] = card.value
-            suits[c] = card.suit
-        x_obs = vals + suits
-        y_obs = tricks[t][p]
-        x_data[4*t+p] = x_obs
-        y_data[4*t+p] = y_obs
-
-x_train = x_data[0:(int(num_rounds*.8))]
-y_train = y_data[0:(int(num_rounds*.8))]
-x_test = x_data[-(num_rounds-int(num_rounds*.8)):]
-y_test = y_data[-(num_rounds-int(num_rounds*.8)):]
-
-nameString = 'tf_ve/Data/Greedy_v_Greedy_bet'
-np.save(nameString + '_x_train', x_train )
-np.save(nameString + '_y_train', y_train )
-np.save(nameString + '_x_test', x_test)
-np.save(nameString + '_y_test', y_test)
+for batch in range(num_batches):
+    dataarray = np.zeros((4*batch_size, x_size+y_size))
+    for t in range(batch_size):
+        #print progress
+        if t%batch_size/10==1:
+            print '{}0 percent of batch complete: batch size is {} and we are on batch {} of {}'.format(t,batch_size, batch, num_batches)
+        #generate a new game
+        game = Game(13, strategies)
+        scores = game.playGame()
+        for p in range(4):
+            #scount tricks taken and get suits and card vals
+            tricks = sum(game.T[t] == p for t in range(13))
+            hand = game.initialHands[p]
+            vals = [0 for i in range(13)]
+            suits = [0 for i in range(13)]
+            for c in range(13):
+                card = hand.cards[c]
+                vals[c] = card.value
+                suits[c] = card.suit
+            #add x and y data to array
+            x_obs = vals + suits
+            y_obs = tricks
+            dataarray[4*t+p,0:x_size] = x_obs
+            dataarray[4*t+p, x_size:x_size+y_size] = y_obs
+    #now ready to append current array
+    with open(nameString, "a") as output:
+        np.savetxt(output, dataarray,delimiter=',')
 
