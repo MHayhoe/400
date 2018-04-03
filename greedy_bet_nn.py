fbc2a30fe188515a70d833e31cdd7f6daf642cc6
Hadi Elzayn <Hadi@Haythams-MacBook-Pro.local>
2018-04-03 00:33:23 -0400

    have a basic nn running though bad results

--- greedy_bet_nn.py
+++ greedy_bet_nn.py
@@ -1,43 +1,45 @@
 from keras import models
 from keras.models import Sequential
-from keras.layers import Dense, Activation
-
+from keras.layers import Dense, Activation,Dropout
+from keras.utils import to_categorical
+import keras
 import numpy as np
-# bets = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Bets.npy')
-# scores = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Scores.npy')
-# hands = np.load('./Data/Greedy_v_Greedy2018-04-02-15-35-2_Hands.npy')
 
-bets = np.load('./Data/Greedy_v_Greedy_Bets.npy')
-scores = np.load('./Data/Greedy_v_Greedy_Scores.npy')
-hands = np.load('./Data/Greedy_v_Greedy_Hands.npy')
-tricks = np.load('./Data/Greedy_v_Greedy_Tricks.npy')
+nameString = './Data/Greedy_v_Greedy_bet'
+x_train = np.load(nameString + '_x_train.npy')
+y_train = np.load(nameString + '_y_train.npy' )
+x_test = np.load(nameString + '_x_test.npy')
+y_test = np.load(nameString + '_y_test.npy')
+#censor ys
+y_train = [min(y_train[i],6) for i in range(len(y_train))]
+y_test = [min(y_test[i],6) for i in range(len(y_test))]
 
-num_rounds = len(hands)
-x_data = [np.array([]) for t in range(num_rounds*4)]
-y_data = [-1 for t in range(num_rounds*4)]
-for t in range(num_rounds):
-    for p in range(4):
-        vals = [0 for i in range(13)]
-        suits = [0 for i in range(13)]
-        for c in range(13):
-            card = hands[t][p].cards[c]
-            vals[c] = card.value
-            suits[c] = card.suit
-        x_obs = vals + suits
-        y_obs = tricks[t][p]
-        x_data[4*t+p] = x_obs
-        y_data[4*t+p] = y_obs
-
-x_train = x_data[0:(int(num_rounds*.8))]
-y_train = y_data[0:(int(num_rounds*.8))]
-x_test = x_data[-(int(num_rounds*.8)+1)]
-y_test = y_data[-(int(num_rounds*.8)+1)]
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 
 
-model = Sequential([
-    Dense(32, input_shape=(26,)),
-    Activation('relu'),
-    Dense(10),
-    Activation('softmax'),
-])
+model = Sequential()
+#add initial layer
+model.add(Dense(13, input_dim=26,activation='relu'))
+model.add(Dropout(.5))
+model.add(Dense(7))
+model.add(Activation('relu'))
+model.add(Dense(4))
+model.add(Activation('relu'))
+model.add(Dense(7))
+model.summary()
+
+sgd = keras.optimizers.SGD()
+opt = keras.optimizers.RMSprop(lr=.01,clipnorm=10.)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+batchsize = 128
+epoches = 20
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs = epoches,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
--- test_strategies.py
+++ test_strategies.py
@@ -2,7 +2,7 @@ from GameObject import Game
 import numpy as np
 import datetime as dt
 import copy
-num_tests = 1000
+num_tests = 1000000
 
 # Let's test heuristic vs random
 total_score_rando = 0
@@ -23,7 +23,9 @@ Scores = []
 Tricks = []
 #games = [Game(13, strategies) for i in range(num_tests)]
 for i in range(num_tests):
-    print i
+    #print i
+    if i% 10000 ==1:
+        print i
     game = Game(13, strategies)
     #game = games[i]
     scores = game.playGame()
@@ -60,7 +62,7 @@ for i in range(num_tests):
     #print Hands
 
 
-print Hands
+#print Hands
 #print Bets
 tempTime = dt.datetime.now().time();
 #timeString = 'Data/Heuristic_v_Greedy' + str(dt.datetime.now().date()) + '-' + str(tempTime.hour) + '-' + str(tempTime.minute) + '-' + str(tempTime.second);
@@ -76,3 +78,35 @@ np.save(timeString + '_Scores', Scores)
 np.save(timeString + '_Tricks', Tricks)
 
 #print 'Heuristic won ' + str(wins_heuristic/num_tests*100) + '% of games, tied ' + str(ties/num_tests*100) + '% of games.'
+
+
+# it is probably better to just convert to data here...
+
+# num_rounds = len(hands)
+# x_data = [np.array([]) for t in range(num_rounds*4)]
+# y_data = [-1 for t in range(num_rounds*4)]
+# for t in range(num_rounds):
+#     if t=
+#     for p in range(4):
+#         vals = [0 for i in range(13)]
+#         suits = [0 for i in range(13)]
+#         for c in range(13):
+#             card = hands[t][p].cards[c]
+#             vals[c] = card.value
+#             suits[c] = card.suit
+#         x_obs = vals + suits
+#         y_obs = tricks[t][p]
+#         x_data[4*t+p] = x_obs
+#         y_data[4*t+p] = y_obs
+#
+# x_train = x_data[0:(int(num_rounds*.8))]
+# y_train = y_data[0:(int(num_rounds*.8))]
+# x_test = x_data[-(int(num_rounds*.8)+1):]
+# y_test = y_data[-(int(num_rounds*.8)+1):]
+#
+# nameString = 'tf_ve/Data/Greedy_v_Greedy_bet'
+# np.save(nameString + '_x_train', x_train )
+# np.save(nameString + '_y_train', y_train )
+# np.save(nameString + '_x_test', x_test)
+# np.save(nameString + '_y_test', y_test)
+#
