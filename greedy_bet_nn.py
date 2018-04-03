cef880feec23824cbced48cdaa292ef96ca88afd
MHayhoe <m.hayhoe@icloud.com>
2018-04-03 17:31:55 -0400

    Added custom loss

--- greedy_bet_nn.py
+++ greedy_bet_nn.py
@@ -1,8 +1,10 @@
 from keras import models
 from keras.models import Sequential
-from keras.layers import Dense, Activation,Dropout
+from keras.layers import Dense, Activation, Dropout
 from keras.utils import to_categorical
 import keras
+from keras import backend as K
+import tensorflow as tf
 import numpy as np
 
 nameString = './Data/Greedy_v_Greedy_bet'
@@ -11,35 +13,44 @@ y_train = np.load(nameString + '_y_train.npy' )
 x_test = np.load(nameString + '_x_test.npy')
 y_test = np.load(nameString + '_y_test.npy')
 #censor ys
-y_train = [min(y_train[i],6) for i in range(len(y_train))]
-y_test = [min(y_test[i],6) for i in range(len(y_test))]
+#y_train = [min(y_train[i],6) for i in range(len(y_train))]
+#y_test = [min(y_test[i],6) for i in range(len(y_test))]
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+#y_train = to_categorical(y_train)
+#y_test = to_categorical(y_test)
 
-
+# Returns our custom loss function
+def get_loss_bet():
+    # Our custom loss function: if we make our bet (y_true >= y_pred), the loss
+    # is the amount we could have gotten if we'd bet y_true, i.e., it's
+    # y_true - y_pred. If we didn't make our bet, then our loss is what we
+    # could have gotten minus what we lost, i.e., y_true + y_pred
+    #(since -1*(-bet) = bet)
+    def loss_bet(y_true, y_pred):
+        return K.mean( y_true + K.sign(y_pred - y_true)*y_pred )
+    return loss_bet
 
 model = Sequential()
 #add initial layer
-model.add(Dense(13, input_dim=26,activation='relu'))
-model.add(Dropout(.5))
-model.add(Dense(7))
-model.add(Activation('relu'))
-model.add(Dense(4))
-model.add(Activation('relu'))
-model.add(Dense(7))
-model.summary()
+model.add(Dense(13, input_dim=26, activation='relu'))
+model.add(Dense(13, activation='relu'))
+#model.add(Dropout(.5))
+model.add(Dense(1, activation='relu'))
+#model.summary()
 
-sgd = keras.optimizers.SGD()
+sgd = keras.optimizers.SGD(lr=.01,clipnorm=10.)
 opt = keras.optimizers.RMSprop(lr=.01,clipnorm=10.)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+model.compile(loss=get_loss_bet(),
+              optimizer=sgd,
+              metrics=[get_loss_bet()])
 batchsize = 128
-epoches = 20
+epoches = 1
 history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
+                    batch_size=batchsize,
                     epochs = epoches,
                     verbose=1,
                     validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, batch_size = batchsize)
+
+pred = model.predict(x_test)
